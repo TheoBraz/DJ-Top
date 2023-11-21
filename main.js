d90d30f8c38f4fb45a62c0d9c2b7b3b6c582c519
TheoBraz <135382477+TheoBraz@users.noreply.github.com>
2023-11-21 16:17:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 song = "";
 var narix = 0;
+var nazoom = 1;
+var scoreLeftWrist = 0;
 
-function preload()
-{
+function preload() {
 	song = loadSound("Alan Walker - The Spectre.mp3");
 }
 
 function setup() {
-	canvas =  createCanvas(600, 500);
+	canvas = createCanvas(600, 500);
 	canvas.center();
 
 	video = createCapture(VIDEO);
@@ -18,24 +19,37 @@ function setup() {
 }
 
 function modelLoaded() {
-  console.log('PoseNet Is Initialized');
+	console.log('PoseNet Is Initialized');
 }
 
 function draw() {
+	if (scoreLeftWrist > 0.6) {
+		play();
+	}
 	image(video, 0, 0, 600, 500);
-	song.setVolume(Math.round(narix/600));
+	song.setVolume((narix / 600));
+	song.rate(nazoom / 250);
 	fill("#FF0000");
 	stroke("#FF0000");
-	document.getElementById("volume").innerHTML = "volume: " + Math.round(narix/600 * 10);
+	document.getElementById("volume").innerHTML = "volume: " + floor(narix / 600 * 5);
+	document.getElementById("speed").innerHTML = "Velocidade: " + floor(nazoom / 250 * 10) / 10;
 }
 
-function gotPoses(results){
-	narix = results[0].pose.nose.x;
-	console.log(narix);
+function gotPoses(results) {
+	if (results.length > 0) {
+		narix = results[0].pose.nose.x;
+		console.log(narix);
+		nazoom = results[0].pose.nose.y;
+		console.log(nazoom)
+		scoreLeftWrist = results[0].pose.keypoints[9].score;
+
+	}
 }
 
-function play()
-{
-	song.play();
-	song.rate(1);
+function play() {
+	if (song.isPlaying()) {
+		song.stop();
+	} else {
+		song.play();
+	}
 }
