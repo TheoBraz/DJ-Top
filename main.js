2bae5e4c666a5f57d3c4076df92de0101c4fb079
TheoBraz <135382477+TheoBraz@users.noreply.github.com>
2023-12-05 16:49:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,71 +1,50 @@
-song = "";
-song2 = "";
-var narix = 0;
-var nazoom = 1;
-var scoreLeftWrist = 0;
-var scoreRightElbow = 0;
+var frutinhasdeliciosas;
+var objectDetector;
+var objetis = [];
+var video;
+var status = false;
 
-function preload() {
-	song = loadSound("Alan Walker - The Spectre.mp3");
-	song2 = loadSound("music.mp3");
+function gotDetections(error, results) {
+    objetis = results;
+    if (results) {
+        console.log(results);
+    }
+    objectDetector.detect(video, gotDetections());
 }
 
-function setup() {
-	canvas = createCanvas(600, 500);
-	canvas.center();
-
-	video = createCapture(VIDEO);
-	video.hide();
-
-	poseNet = ml5.poseNet(video, modelLoaded);
-	poseNet.on('pose', gotPoses);
+function preload() {
+    frutinhasdeliciosas = loadImage("frutasdeliciosas.jpg")
 }
 
 function modelLoaded() {
-	console.log('PoseNet Is Initialized');
+    status = true;
+    console.log('Model Loaded!');
+    objectDetector.detect(video, gotDetections());
+}
+
+function setup() {
+    canvas = createCanvas(1450, 650);
+    video = createCapture(VIDEO);
+    video.size(1450, 650);
+    video.hide();
+    canvas.center();
+}
+function borabil() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("butaum").innerHTML = "Detectando Objetos";
 }
 
 function draw() {
-	if (scoreLeftWrist > 0.6) {
-		song2.stop();
-		song.play();
-		if (song.isPlaying()) {
-			song.stop();
-		} else {
-			song.play();
-		}
-	}
-	if (scoreRightElbow > 0.7) {
-		song.stop();
-		song2.play();
-		if (song2.isPlaying()) {
-			song2.stop();
-		} else {
-			song2.play();
-		}
-	}
-	image(video, 0, 0, 600, 500);
-	song.setVolume((narix / 600));
-	song.rate(nazoom / 250);
-	fill("#FF0000");
-	stroke("#FF0000");
-	document.getElementById("volume").innerHTML = "volume: " + floor(narix / 600 * 5);
-	document.getElementById("speed").innerHTML = "Velocidade: " + floor(nazoom / 250 * 10) / 10;
-}
-
-function gotPoses(results) {
-	console.log(results);
-	if (results.length > 0) {
-		narix = results[0].pose.nose.x;
-		console.log(narix);
-		nazoom = results[0].pose.nose.y;
-		console.log(nazoom)
-		scoreLeftWrist = results[0].pose.keypoints[9].score;
-		scoreRightElbow = results[0].pose.keypoints[8].score;
-
-	}
-}
-
-function play() {
-
+    image(video, 0, 0, 1450, 650);
+    fill("#FF0000");
+    if (status == true) {
+        objectDetector.detect(video, gotDetections());
+        for (i = 0; i < objetis.length; i++) {
+            fill("FF0000");
+            text(objetis[i].label, objetis[i].x, objetis[i].y);
+            noFill();
+            stroke(0, 255, 0);
+            rect(objetis[i].x, objetis[i].y, objetis[i].width, objetis[i].height);
+        }
+    }
 }
